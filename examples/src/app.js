292988db526e84abe3282f602f49b9294c4b3235
HVT11 <huynhtri11102000@gmail.com>
2022-06-06 18:20:53 +0700

    Example for variables, types, expressions, operators.

--- examples/src/app.js
+++ examples/src/app.js
@@ -1,1 +1,50 @@
-console.log('Hello world!')
+//VARIABLES
+const PI = 3.14 //No change value
+
+let a = 0 //Can change value
+a = 1
+
+var b = 1
+
+//TYPES
+const typeNumber = 12 //Primitive Types
+
+//Object types
+const people = {
+    name: 'John',
+    age: 12
+}
+
+//Expressions
+2
+0.02
+'something'
+true
+this
+undefined
+
+//OPERATORS
+// The assignment operator (=)
+let x = 1
+
+// The addition operator (+)
+const three = 1 + 2
+three + 1 //4
+'three' + 1 //three1
+
+// The subtraction operator (-)
+const two = 4 - 2 // 2
+
+// The division operator (/)
+20 / 5 // 4
+1 / 0 //Infinity
+
+// The remainder operator (%)
+20 % 5 //result === 0
+1 % 0 //NaN
+
+// The multiplication operator (*)
+1 * 2 //2
+
+// The exponentiation operator (**)
+2 ** 8 //256
